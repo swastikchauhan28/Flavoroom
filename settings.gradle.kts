3244b10348f478eea7b254c85cac3f4d6a147dbb
Swastik <swastikchauhan93@gmail.com>
2025-03-17 19:41:10 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        maven(url = "https://jitpack.io") // Correct way to define Maven URL
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.application") version "8.5.1"
+        id("org.jetbrains.kotlin.android") version "1.9.10"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io") // Ensure JitPack is here too
+    }
+}
+
+rootProject.name = "Flavoroom"
+include(":app")
+
+
+ 
